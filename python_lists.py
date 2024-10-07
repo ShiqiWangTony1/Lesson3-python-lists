82c6138060e289e51e4bcc6d63f0ceb3b4bbada3
ShiqiWangTony1 <shiqi.wang0728@gmail.com>
2024-10-07 18:15:44 -0400

    Lesson 3 python lists

---
+++ python_lists.py
@@ -0,0 +1,29 @@
+# Task 1: Given the list of grades:
+# grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+# sort_grade = sorted(grades)[::-1]
+# print(sort_grade)
+
+# Task 2: Calculate the average grade and display it.
+# avg_grade = sum(grades) / len(grades)
+# print(avg_grade)
+
+
+# 2.
+# Task 1: Given the two lists
+# submitted = ["Alice", "Bob", "Charlie", "David"]
+# attended = ["Charlie", "Eve", "Alice", "Frank"]
+# both_students = []
+# for student in submitted:
+#     if student in attended:
+#         both_students.append(student)
+# print(both_students)
+
+# # 3.
+# # Task 1
+# temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+# second_week_temp = temperatures[7:14]
+# print(second_week_temp)
+
+# # Task 2
+# temp_above_100 = [temp for temp in temperatures if temp > 100]
+# print(temp_above_100)
